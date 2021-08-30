74737a4d82e2ee537c46f9fb31a0b61bc32cfab3
Marco <marcoparra311@gmail.com>
2021-08-30 18:12:37 -0400

    Fix linters atttempt #2

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import navStyles from '../../styles/navStyles.css';
+import '../../styles/navStyles.css';
 
 function Nav() {
   return (
