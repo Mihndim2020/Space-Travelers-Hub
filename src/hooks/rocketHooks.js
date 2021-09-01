e651ed69c4d227ab30a75c3c0071f064bff342b0
Marco <marcoparra311@gmail.com>
2021-09-01 15:57:53 -0400

    Style navbar

--- src/hooks/rocketHooks.js
+++ src/hooks/rocketHooks.js
@@ -1,12 +1,15 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getRocketsAction } from '../redux/rockets/rockets';
 
 const rocketHooks = () => {
   const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets);
 
   useEffect(() => {
-    dispatch(getRocketsAction());
+    if (!rockets.length) {
+      dispatch(getRocketsAction());
+    }
   }, []);
 };
 
